d39e7aeb78e80d6bfb5918e2974452cfc223bfb0
Keinher Iribarren <iribarrenkeinher@gmail.com>
2025-07-20 01:17:41 +0200

    refactor loader logic

--- src/js/loader.js
+++ src/js/loader.js
@@ -4,13 +4,15 @@ window.addEventListener('load', async () => {
     const typedName = document.getElementById('typed-name');
     const typedTitle = document.getElementById('typed-title');
     const cursor = document.getElementById('cursor');
+    const loaderContainer = document.getElementById('loader');
     const cursorTwo = document.getElementById('cursor-two');
     const toggleInit = document.getElementById('loader-toggle');
-
+    let lastScrollTop = 0;
     const name = 'Keinher Iribarren';
     const title = '<Full Stack Developer/>';
     let index = 0;
     let playing = false;
+    let blockScroll = true;
     
     function playTypingSound() {
         if (playing) audioTyping.pause();
@@ -41,6 +43,7 @@ window.addEventListener('load', async () => {
             if (forTitle) {
                 playTypingSound();
                 cursorTwo.style.display = 'none';
+                document.getElementsByTagName('body')[0].style.overflow = 'auto'; 
             } else {
                 typedTitle.style.display = 'inline-block';
                 cursor.style.display = 'none';
@@ -54,7 +57,7 @@ window.addEventListener('load', async () => {
     toggleInit.addEventListener('click', () => {
         toggleInit.classList.remove('fade-in');
         toggleInit.classList.remove('pulse');
-        toggleInit.classList.add('slide-up-out') 
+        toggleInit.classList.add('slide-up-out')
         playTypingSound();
     });
 
@@ -71,4 +74,24 @@ window.addEventListener('load', async () => {
     toggleInit.addEventListener('mouseleave', () => {
         toggleInit.classList.add('pulse');
     });
+
+    loaderContainer.addEventListener('animationend', (a) => {
+        if (a.animationName != 'acordion') return;
+        blockScroll = false;
+        window.removeEventListener('scroll', () => {});
+    });
+
+    window.addEventListener('scroll', (e) => {
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        const isDevice = window.matchMedia("(max-width: 768px)").matches;
+        
+        if (isDevice) return;
+
+        if (currentScroll > lastScrollTop && blockScroll) {
+            loaderContainer.classList.add('acordion');
+            window.scrollTo(0, 0);
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    });    
 })
