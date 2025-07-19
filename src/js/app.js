f1f016b668be86f2a39b00bb2193b0cb190031de
Keinher Iribarren <iribarrenkeinher@gmail.com>
2025-07-20 01:18:13 +0200

    link new modules and new cursor logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     loadComponent('#loader', './modules/loader.html');
+    loadComponent('#aside', './modules/aside.html');
+    loadComponent('#about', './modules/about.html');
+    loadComponent('#experience', './modules/experience.html');
+    loadComponent('#skills', './modules/skills.html');
     
     const cursor = document.getElementById('light-cursor');
+    const isDevice = window.matchMedia("(max-width: 768px)").matches;
     
     document.addEventListener('mousemove', (e) => {
-        cursor.style.left = e.clientX + 'px';
-        cursor.style.top = e.clientY + 'px';
+        if (isDevice) return;
+        cursor.style.background = `radial-gradient(600px at ${e.clientX}px ${e.clientY}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
     });
+    function moveRandomRadialGradient() {
+        const randomX = Math.floor(Math.random() * window.innerWidth);
+        const randomY = Math.floor(Math.random() * window.innerHeight);
+        cursor.style.background = `radial-gradient(600px at ${randomX}px ${randomY}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
+        setTimeout(() => {
+            moveRandomRadialGradient();
+        }, 1000);
+    }
+
+    if (isDevice) moveRandomRadialGradient();
 });
 
