fa2e379693d05008c4eef733876bfe575790f64d
Keinher Iribarren <iribarrenkeinher@gmail.com>
2025-07-20 15:50:33 +0200

    chhange dataset and module logic

--- src/js/modules.js
+++ src/js/modules.js
@@ -2,15 +2,21 @@ window.addEventListener('load', async () => {
     const data = await fetch('../src/dataset.json');
     console.log('dataset',data);
     const json = await data.json();
+    console.log('json',json);
+    const experiences = json?.experiences ?? [];
+    const skills = json?.skills ?? [];
+
 
     const experienceList = document.getElementById('experience-list');
-    let template = '';
-    json.forEach(item => {
-        template +=
+    const skillsList = document.getElementById('skills-list');
+    let templateExperiences = '';
+    experiences.forEach(item => {
+        templateExperiences +=
         `
             <section class="experience-item">
                 <div class="experience-item-period">${item.from} - ${item.to}</div>
                 <div class="experience-item-title">${item.title}</div>
+                <div class="experience-item-company">${item.company}</div>
                 <p>${item.description}</p>
                 <div class="experience-item-skills">
                     <ul>
@@ -21,6 +27,25 @@ window.addEventListener('load', async () => {
         `
     })
 
+    let templateSkills = '';
+    skills.forEach(skill => {
+        let templateLevels = ``;
+        for (let i = 1; i <= 6; i++) {
+            templateLevels += `<span class="skill-level-item ${i <= skill.level ? 'active' : '' }"></span>`;
+        } 
 
-    experienceList.innerHTML = template;
+        templateSkills +=
+        `
+            <div class="skill-item">
+                <div class="skill-item-name">${skill.name}</div>
+                <div class="skill-levels">
+                    ${templateLevels}
+                </div>
+            </div>
+        `
+    })
+
+    skillsList.innerHTML = templateSkills;
+
+    experienceList.innerHTML = templateExperiences;
 });
