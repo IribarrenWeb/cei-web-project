e7c81c87b4616a5f15c9919523f4b94f984bc341
Keinher Iribarren <iribarrenkeinher@gmail.com>
2025-07-20 14:23:54 +0200

    fix animation for device

--- src/js/app.js
+++ src/js/app.js
@@ -6,21 +6,45 @@ document.addEventListener('DOMContentLoaded', () => {
     loadComponent('#skills', './modules/skills.html');
     
     const cursor = document.getElementById('light-cursor');
+    const cursor2 = document.getElementById('light-cursor-two');
     const isDevice = window.matchMedia("(max-width: 768px)").matches;
-    
+    let cursorSwitch = false;
+
     document.addEventListener('mousemove', (e) => {
         if (isDevice) return;
         cursor.style.background = `radial-gradient(600px at ${e.clientX}px ${e.clientY}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
     });
+
     function moveRandomRadialGradient() {
+        cursorSwitch = !cursorSwitch;
+        const cursorToMove = cursorSwitch ? cursor2 : cursor;
         const randomX = Math.floor(Math.random() * window.innerWidth);
         const randomY = Math.floor(Math.random() * window.innerHeight);
-        cursor.style.background = `radial-gradient(600px at ${randomX}px ${randomY}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
-        setTimeout(() => {
-            moveRandomRadialGradient();
-        }, 1000);
+        cursorToMove.style.background = `radial-gradient(600px at ${randomX}px ${randomY}px, rgba(29, 78, 216, 0.15), transparent 90%)`;
+
+        if (!cursorSwitch) {
+            switchAnimations(cursor2, true);
+            switchAnimations(cursor, false);
+        } else {
+            switchAnimations(cursor, true);
+            switchAnimations(cursor2, false);
+        }
+        
+        setTimeout(() => moveRandomRadialGradient(), 2500);
     }
 
-    if (isDevice) moveRandomRadialGradient();
+    function switchAnimations(element, isOut = false) {
+        if (isOut) {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        } else {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        }
+    }
+
+    if (isDevice) {
+        moveRandomRadialGradient();
+    }
 });
 
