eeafea90266a81991014b72ea351f1dbf6401696
Keinher Iribarren <iribarrenkeinher@gmail.com>
2025-07-26 15:20:08 +0200

    refactor loader logic

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,78 +1,108 @@
 window.addEventListener('load', async () => {
     await delay(500)
     const audioTyping = document.getElementById('audio-typing');
-    const typedName = document.getElementById('typed-name');
-    const typedTitle = document.getElementById('typed-title');
+    const typedContainer = document.getElementById('typed-container');
     const cursor = document.getElementById('cursor');
+    const body = document.querySelector('body');
     const loaderContainer = document.getElementById('loader');
-    const cursorTwo = document.getElementById('cursor-two');
     const toggleInit = document.getElementById('loader-toggle');
     let lastScrollTop = 0;
-    const name = 'Keinher Iribarren';
-    const title = '<Full Stack Developer/>';
-    let index = 0;
+    const words = [
+        'Hello World!',
+        'I\'m Keinher Iribarren',
+        'a Full Stack Developer',
+        'and this is my CV',
+        'startPage(pagetype);',
+    ];
     let playing = false;
     let blockScroll = true;
     
-    function playTypingSound() {
-        if (playing) audioTyping.pause();
-        else {
-            playing = true;
-            audioTyping.currentTime = 0;
-            audioTyping.volume = 0.5;
-            audioTyping.play();
-        }
-    }
+    async function init() {
+        let wordsProcessed = 0;
+        do {
+            if (wordsProcessed == 0) playTypingSound();
 
-    function typeLetter(forTitle = false) {
-        const toType = forTitle ? title : name;
-
-        if (index < toType.length) {
-            if (forTitle) typedTitle.textContent += toType[index];
-            else typedName.textContent += toType[index];
-
-            index++;
-            
-            let speed = Math.random() * 300;
-            
-            if (speed == 0) speed = 150;
-
-            setTimeout(() => typeLetter(forTitle), speed);
-        } else {
-            index = 0;
-            if (forTitle) {
+            const word = words[wordsProcessed];
+            const isLastWord = wordsProcessed == (words.length - 1);
+            if (isLastWord) {
                 playTypingSound();
-                cursorTwo.style.display = 'none';
-                document.getElementsByTagName('body')[0].style.overflow = 'auto'; 
-            } else {
-                typedTitle.style.display = 'inline-block';
-                cursor.style.display = 'none';
-                cursorTwo.style.display = 'inline-block';
-                typeLetter(true);
+                await delay(1500)
+                playTypingSound();
             }
+            await typeWord(word,  isLastWord ? 'code' : 'span');
+
+            wordsProcessed++;
+            console.log('wordsProcessed', wordsProcessed, words.length)
+            if (wordsProcessed == words.length) playTypingSound();
+
+        } while (words.length > wordsProcessed)
+
+        loaderContainer.classList.add('acordion')
+        body.style.overflowY = 'auto';
+    }
+
+    async function typeWord(word, tag = 'span') {
+        const newContainerMain = document.createElement('div')
+        const newContainerWord = document.createElement(tag)
+        const isCode = tag == 'code';
+        let newCodeScript = null;
+        // if (tag == 'code') {
+        //     newContainerWord.style.fontSize = '1em';
+        // }
+        newContainerMain.style.display = 'flex';
+        newContainerMain.style.alignItems = 'center';
+        newContainerMain.appendChild(newContainerWord);
+        if (isCode) {
+            const newCodeCursor = document.createElement('span')
+            newCodeScript = document.createElement('pre')
+            newCodeCursor.appendChild(cursor)
+            newContainerWord.appendChild(newCodeScript)
+            newContainerWord.appendChild(newCodeCursor)
+            await delay(500)
+        } else newContainerMain.appendChild(cursor) 
+        
+
+        typedContainer.appendChild(newContainerMain);
+
+        for (let index = 0; index < word.length; index++) {
+            const letter = word[index];
+            
+            let speed = Math.random() * 250;
+            
+            if (speed == 0) speed = 100;
+
+            await delay(speed);
+
+            if (isCode) newCodeScript.innerHTML += letter;
+            else newContainerWord.innerHTML += letter;
+        
         }
     }
 
+    function playTypingSound() {
+        try {
+            if (playing) {
+                audioTyping.pause();
+                playing = false;
+            } else {
+                playing = true;
+                audioTyping.currentTime = 1;
+                audioTyping.loop = true;
+                audioTyping.playbackRate = 0.9;
+                audioTyping.volume = 0.5;
+                audioTyping.play();
+            }
+        } catch (error) {
+            console.error(error)            
+        }
+    }
 
     toggleInit.addEventListener('click', () => {
-        toggleInit.classList.remove('fade-in');
-        toggleInit.classList.remove('pulse');
-        toggleInit.classList.add('slide-up-out')
-        playTypingSound();
-    });
-
-    toggleInit.addEventListener('animationend', () => {
         toggleInit.style.display = 'none';
-        document.getElementById('typed-container').style.display = 'flex';
-        typeLetter();
-    });
-
-    toggleInit.addEventListener('mouseenter', () => {
-        toggleInit.classList.remove('pulse');
-    });
-
-    toggleInit.addEventListener('mouseleave', () => {
-        toggleInit.classList.add('pulse');
+        typedContainer.style.display = 'flex';
+        delay(1500).then(()=> {
+            init()
+        })
     });
 
     loaderContainer.addEventListener('animationend', (a) => {
